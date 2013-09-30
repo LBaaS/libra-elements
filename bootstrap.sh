a97bcb37aa79885c99434feb227f408d4673bd81
Endre Karlson <endre.karlson@gmail.com>
2013-09-30 02:11:34 +0200

    make the script actually become useful

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,85 @@
 #!/bin/bash
 
+set -x
+
 # A helper script to do all the necassary actions to actually get ready for building images.
 
 dist=`lsb_release -is`
 
-case $dist in
-    Ubuntu)
-        apt-get update -q
-        apt-get install -qy git qemu kpartx
+install_deps() {
+    case $dist in
+        Ubuntu)
+            apt-get update -q
+            apt-get install -qy git qemu kpartx
+        ;;
+        Fedora)
+            yum update -y
+            yum install -y git qemu kpartx
+        ;;
+    esac
+}
+
+
+git_clone() {
+    local src=$1
+    local target=$2
+
+    [ ! -d $target ] && {
+        git clone $src $target
+    } || {
+        cd $target
+        git pull origin master
+    }
+}
+
+
+clone_repos() {
+    reload_env
+    git_clone https://github.com/openstack/diskimage-builder $DIB_PATH
+    git_clone https://github.com/LBaaS/libra-elements $LIBRA_ELEMENTS
+}
+
+
+add_export() {
+    echo ""
+    #[ -z "$"]
+    local string="$1"
+
+}
+
+setup_profile() {
+    cat >$HOME/.dib_profile<<EOF
+export DIB_PATH=\$HOME/diskimage-builder
+export PATH=$PATH:\$DIB_PATH/bin
+export LIBRA_ELEMENTS=\$HOME/libra-elements
+export ELEMENTS_PATH=\$DIB_PATH/elements:\$LIBRA_ELEMENTS/elements
+EOF
+    echo "Now ready, please do:"
+    echo "source $HOME/.bashrc"
+
+    [ -z "$(egrep dib_profile $HOME/.bashrc)" ] && {
+        echo "source $HOME/.dib_profile" >> $HOME/.bashrc
+    }
+}
+
+reload_env() {
+    . ~/.bashrc
+}
+
+case $1 in
+    deps)
+        install_deps
     ;;
-    Fedora)
-        yum update -y
-        yum install -y git qemu kpartx
+    profile)
+        setup_profile
+    ;;
+    clone)
+        clone_repos
+    ;;
+    *)
+        install_deps
+        setup_profile
+        reload_env
+        clone_repos
     ;;
 esac
-
-echo 'export DIB_PATH=$HOME/diskimage-builder' >> ~/.bashrc
-echo 'export PATH=$PATH:$DIB_PATH/bin' >> ~/.bashrc
-echo 'export LIBRA_ELEMENTS=$HOME/libra-elements' >> ~/.bashrc
-
-echo 'export ELEMENTS_PATH=$DI_BUILDER_PATH/elements:$LIBRA_ELEMENTS' >> ~/.bashrc
-
-. ~/.bashrc
-
-git clone https://github.com/openstack/diskimage-builder $DIB_PATH
-git clone http://github.com/lbaas/libra-elements $LIBRA_ELEMENTS
-
-echo "Now ready, please do\n. ~/.bashrc"
